05592ffe6c84f623a3cbf2ce5964d72308d19821
tuzzm <tuzzm@DESKTOP-2O1E47R>
2016-08-24 10:29:26 +1200

    1.6

--- src/main/java/co/tuzza/swipehq/SwipeHQClient.java
+++ src/main/java/co/tuzza/swipehq/SwipeHQClient.java
@@ -45,7 +45,7 @@ import java.util.Map;
  */
 public class SwipeHQClient {
 
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.6";
 
     private final Map<String, String> params = new LinkedHashMap();
     private final HttpTransport transport;
